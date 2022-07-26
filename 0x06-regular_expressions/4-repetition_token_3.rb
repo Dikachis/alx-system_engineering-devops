085fc1c59ede0afd0be6ffea3509accceacd7772
Dikachis <dikachi015safety@gmail.com>
2022-07-26 22:52:10 +0100

    Added 4-repetitio file

--- 0x06-regular_expressions/4-repetition_token_3.rb
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 # Repetition Token #3
 
-puts ARGV[0].scan(/^hbt+n/).join
-# puts ARGV[0].scan(/hbt*n/).join
+# puts ARGV[0].scan(/^hbt+n/).join
+# OR
+puts ARGV[0].scan(/hbt*n/).join
