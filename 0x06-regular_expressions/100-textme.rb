0529d1223c5e9b5b9a7528b067a0554845a77929
Dikachis <dikachi015safety@gmail.com>
2022-07-26 23:23:36 +0100

    Added  100-textme file

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -4,6 +4,8 @@ Format = ARGV[0].scan(/from:(.\w+)|to:(.\w+)|flags:([0-9:-]+)/)
 List = [Format[0].compact, Format[1].compact, Format[2].compact]
 puts List.join(',')
 
+#OR
+
 #puts ARGV[0]
 #  .scan(/(?<=from:|to:|flags:).*?(?=\])/)
 # .join(',')
