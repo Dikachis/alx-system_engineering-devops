09cefb4b76d0cfe16792d48a7570a1c0eb136e80
Dikachis <dikachi015safety@gmail.com>
2022-09-25 18:27:23 +0000

    Fixed some bugs

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,6 +1,34 @@
-# Installs a Nginx server
+# configures nginx on a server
+$config = "server {
+	listen 80 default_server;
+        listen [::]:80 default_server;
 
-exec {'install':
-  provider => shell,
-  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/Unique-red permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
+        root /var/www/html;
+        index index.html;
+
+        location /redirect_me {
+                return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+        }
+
+}"
+
+package { 'nginx':
+ensure	=> 'installed',
+}
+
+file { 'index.html':
+ensure	=> 'present',
+path	=> '/var/www/html/index.html',
+content	=> 'Hello World!',
+mode	=> '0644'
+}
+
+file { 'server_config':
+ensure	=> 'present',
+path 	=> '/etc/nginx/sites-available/default',
+content => $config
+}
+
+exec { 'service nginx restart':
+path	=> ['/usr/sbin', '/usr/bin']
 }
